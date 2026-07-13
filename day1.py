dad90ffa052e9ab5444aa098eee3cb8639ef2cfa
AriadnaDev <avilavillegasariadna@gmail.com>
2026-07-13 16:49:29 +0200

    Script día 1 completado - Tipos de dato e imprimir por pantalla

---
+++ day1.py
@@ -0,0 +1,35 @@
+#Se puede ejecutar en terminal directamente escribiendo python y para salir utilizando exit()
+
+"""Hola, soy un comentario multilínea
+¿ves?
+qué magia"""
+
+
+#Ejercicios -----------------------------
+#1. Comprobar versión Python (python --version en cmd) -> 3.13.7
+
+#2. Realizar varias operaciones
+#Operadores matemáticos
+""" 
+Suma: 2+3, 
+Resta: 3-2,
+Multiplicación: 2*3,
+División: 3/2,
+Resto módulo: 3%2,
+Resto entero:3//2,
+Potencia: 3**2,
+..."""
+
+#3. Imprimir varios datos
+print("Hola, qué recuerdos me da Python", True, 9.99, "qué buena idea este reto");
+
+#4. Comprobar el tipo de dato
+print(type(10)); #entero
+print(type(3.14)); #flotante
+print(type(1+3j)); #complejo
+print(type("Ari")); #string/caracter
+print(type([1,2,3])); #lista
+print(type({"nombre":"Ari"})); #diccionario
+print(type({9.18, 3.14, 2.7})); #Set/conjunto
+print(type((9.8, 3.14, 2.7))); #tupla
+
